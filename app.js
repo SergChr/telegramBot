28183664c3e4d13b00f21b03d727417e030f7319
SergChr <style.live@yandex.ua>
2017-06-09 19:03:33 +0300

    delete https in app.js, added preccess_env_port

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-let https = require('https'),
-    fs = require('fs'),
-    cmd = require("./scripts/cmd"),
-    config = require("config");
-
+//let https = require('https'),
+  //  fs = require('fs'),
+  // cmd = require("./scripts/cmd"),
+   let config = require("config"),
+    express = require("express"),
+    app = express();
+/*
 // certificates for https
 let options = {
     key: fs.readFileSync('./cert/key.pem'),
     cert: fs.readFileSync('./cert/cert.pem'),
     passphrase: config.get("cert_pass")
 };
-
+*/
+/*
 https.createServer(options, function (req, res) {
     res.writeHead(200);
     res.end('Running...\n');
 }).listen(443, () => {
+    console.log("Server running...");
+});
+*/
+
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server running...");
 });
