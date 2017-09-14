e30015ba662c45afbd546aab2bd9d564d4c099fb
SergChr <style.live@yandex.ua>
2017-09-14 21:41:03 +0300

    Clear garbage

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-const https = require('https'),   
-    fs = require('fs'),
-    cmd = require("./scripts/cmd"),
+const cmd = require("./scripts/cmd"),
     config = require("config"),
     express = require("express"),
     app = express();
-/*
-// certificates for https
-let options = {
-    key: fs.readFileSync('./cert/key.pem'),
-    cert: fs.readFileSync('./cert/cert.pem'),
-    passphrase: config.get("cert_pass")
-};
-https.createServer(options, function (req, res) {
-    res.writeHead(200);
-    res.end('Running...\n');
-}).listen(443, () => {
-    console.log("Server running...");
-});
-*/
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server running...");
